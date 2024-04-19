3ed64341bc0458b72d02481f6c7c69026cdcf422
Rohan Sampat <rsampat2@id.iit.edu>
2024-04-19 05:34:27 -0400

    coloured nodes

--- programming_files/src/network/visuals.py
+++ programming_files/src/network/visuals.py
@@ -1,27 +1,28 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 from graph_initializer import create_graph ,positive_edges, negative_edges
-from identify import find_max_cliques
+from network.identify import find_max_cliques, map_nodes_to_cliques
 
 def plot_graph(G, pos, old_pos, force_vectors, ax):
     ax.clear()
-    nx.draw(G, pos, ax=ax, node_color='black', node_size=50)
+    # nx.draw(G, pos, ax=ax, node_color='black', node_size=50)
 
 
     # Drawing nodes
     # nx.draw_networkx_nodes(G, pos, node_size=50)
 
+    cliques = find_max_cliques(G)  # Retrieve the list of cliques
+    node_to_clique = map_nodes_to_cliques(G)  # This is the mapping from nodes to clique indices
+
+    # Initialize the colormap
     colormap = plt.cm.get_cmap('Pastel2_r', len(cliques))
 
-    # Create a color map for nodes based on their clique membership
-    node_color = {}
-    for idx, clique in enumerate(cliques):
-        clique_color = colormap(idx / len(cliques))  # Normalize idx to the range [0, 1]
-        for node in clique:
-            node_color[node] = clique_color
+    # Create a color map for nodes based on their clique index
+    node_color = {node: colormap(node_to_clique[node] / len(cliques)) for node in G.nodes()}
 
-    # Now draw the graph with these node colors
-    nx.draw(G, pos, node_color=[node_color[node] for node in G.nodes()], with_labels=True)
+    # Drawing nodes with colors mapped by cliques
+    nx.draw(G, pos, ax=ax, node_color=[node_color.get(node, 'black') for node in G.nodes()],
+            node_size=50, with_labels=False)
 
 
     # Drawing edges
@@ -38,4 +39,3 @@ def plot_graph(G, pos, old_pos, force_vectors, ax):
 
     for (i, j), vector in force_vectors['attractive'].items():
         ax.quiver(pos[i][0], pos[i][1], vector[0], vector[1], angles='xy', scale_units='xy', scale=1, color='blue', width=0.002, alpha=0.5)
-
